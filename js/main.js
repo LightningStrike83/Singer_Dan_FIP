8ef239210b9ecd605e6571ac4b03a8e7a7e252db
LightningStrike83 <113542893+LightningStrike83@users.noreply.github.com>
2023-04-03 00:23:11 -0400

    Hamburger Menu, Responsiveness
    Added functionality for hamburger menu, and some extra responsiveness features

---
+++ js/main.js
@@ -0,0 +1,10 @@
+console.log ('Send your tastebuds into orbit!');
+
+    let button = document.querySelector("#burger_button");
+	let burgerCon = document.querySelector("#burger_con");
+
+	function hamburgerMenu() {
+		burgerCon.classList.toggle("display");
+	};
+
+	button.addEventListener("click", hamburgerMenu);
